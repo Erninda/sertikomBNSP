fc12ee74cedb8ecea87dcef87f8901132f9c2273
Erninda <erninda.ristiani94@gmail.com>
2021-10-26 00:47:11 +0700

    commit

---
+++ application/views/sertifikasi/about.php
@@ -0,0 +1,30 @@
+
+        <div class="col-md-10 mt-5">
+          <h2>ABOUT</h2>
+          
+          <div class="row mt-3">
+            <div class="col-md-2">
+              <img src="<?= base_url()?>/assets/img/Erninda.JPG" width="200px" length="500px" alt="">
+            </div>
+            <div style="margin-left: 5%">
+            <div class="col-md-15">
+              <br/>
+              <br/>
+              <br/>
+              <h4>Aplikasi ini dibuat oleh : </h4>
+              <br/>
+              <h4>Nama : Erninda Ristiani</h4>
+              <h4>NIM : 1831710001</h4>
+              <h4>Tanggal : 25 Oktober 2021</h4>
+            </div>
+            </div>  
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+    
+
+
+
